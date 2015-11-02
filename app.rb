3259fce67769f960dae76f8ae187961bf795dd15
arshkhn2 <arshkhn2@hotmail.com>
2015-11-02 15:59:47 +0000

    cheating prevention

--- app.rb
+++ app.rb
@@ -81,13 +81,23 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/win' do
+    if @game.check_win_or_lose==:win
+      erb :win # You may change/remove this line
+    else
+      redirect '/show'
+    end
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    
   end
   
   get '/lose' do
+    if @game.check_win_or_lose==:lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    
   end
   
 end
